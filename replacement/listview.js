cb1b16821dca2da5e9f837f0d16c46ab237689e0
oldkingOK <oldkingok@qq.com>
2024-06-22 21:55:09 +0800

    docs(listview.js): Update comment

--- replacement/listview.js
+++ replacement/listview.js
@@ -1,6 +1,6 @@
 var o_getListItemHtml = getListItemHtml;
 var getListItemHtml = function(item, index, options) {
-    // 格式化html，防止html标签被转义
+    // 格式化html，例如 &lt;br&gt; -> <br>
     let tmpResult = o_getListItemHtml.apply(this, [item, index, options]);
     if (item.Type != "LyricsLine") return tmpResult
     let tmpTextArea = document.createElement('textarea');
